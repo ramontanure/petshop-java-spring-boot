c1459f77a440cc01cc7c0696a2a065509ee2f2c5
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2022-11-19 11:19:54 -0300

    Implementação da classe de modelo Categoria e Rest

---
+++ src/main/java/com/ramon/petshop/domain/Categoria.java
@@ -0,0 +1,60 @@
+package com.ramon.petshop.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	//Construtor é o que inicializa a classe
+	//Contrutor vazio
+	public Categoria() {
+		
+	}
+	
+	//Construtor com campos
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	//GET e Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
+	
+}
