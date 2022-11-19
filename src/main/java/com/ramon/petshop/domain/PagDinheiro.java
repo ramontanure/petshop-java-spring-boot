972e848c48927e4253930a4194f11c49d1ea415c
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2022-11-19 16:18:03 -0300

    Criar Servico, Situacao e Tipos de Pagamento, criar o relacionamento de Heranças e Pagamento

---
+++ src/main/java/com/ramon/petshop/domain/PagDinheiro.java
@@ -0,0 +1,44 @@
+package com.ramon.petshop.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.ramon.petshop.domain.enuns.SituacaoPagamento;
+
+@Entity
+public class PagDinheiro extends Pagamento {
+
+	private static final long serialVersionUID = 1L;
+
+	private Date dataPagamento;
+	private Double desconto;
+
+	public PagDinheiro() {
+
+	}
+
+	public PagDinheiro(Integer id, Double valor, SituacaoPagamento situacao, Servico servico, Date dataPagamento,
+			Double desconto) {
+		super(id, valor, situacao, servico);
+		this.dataPagamento = dataPagamento;
+		this.desconto = desconto;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
+	}
+
+}
