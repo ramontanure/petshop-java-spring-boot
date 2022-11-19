aa84d1a2f9584c360f802dca6fb2dc5f68a83fd6
Ramon Tanure <ramon_tanure_carvalho@hotmail.com>
2022-11-19 11:57:39 -0300

    Configuração banco de dados H2

--- src/main/java/com/ramon/petshop/domain/Categoria.java
+++ src/main/java/com/ramon/petshop/domain/Categoria.java
@@ -3,10 +3,21 @@ package com.ramon.petshop.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//Para espelhar essa classe no banco de dados devemos marcar com entity
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	//Chave primiaria
+	//Estrategia para gerar automaticamente a chave
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
